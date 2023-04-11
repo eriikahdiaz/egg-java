46f1ceb068159cc2251dbc8b8abda733fec4c210
Erikah <erikah@Joses-MacBook-Pro.local>
2023-04-10 21:29:21 -0300

    subo este archivo

---
+++ ejercicio3/src/main/java/com/eriikah/ejercicio3/Ejercicio3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.eriikah.ejercicio3;
+
+/**
+ *
+ * @author erikah
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
